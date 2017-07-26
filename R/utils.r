68ab784847741e4cbe87edff88dcadeb963511dc
gfellis <gfellis@umn.edu>
2017-07-26 16:03:14 -0500

    Pull out find files in zip file logic

--- R/utils.r
+++ R/utils.r
@@ -7,3 +7,22 @@ select_var_rows <- function(df, vars, filter_var = "var_name") {
   }
   df
 }
+
+
+find_files_in_zip <- function(file, name_ext = NULL, name_regex = NULL, multiple_ok = FALSE) {
+  file_names <- utils::unzip(file, list = TRUE)$Name
+
+  if (!is.null(name_ext)) file_names <- stringr::str_subset(file_names, paste0("\\.", name_ext, "$"))
+
+  if (!is.null(name_regex)) file_names <- stringr::str_subset(file_names, name_regex)
+
+  if (!multiple_ok && length(file_names) > 1) {
+    arg_name <- deparse(substitute(name_regex))
+    stop(paste0(
+      "Multiple files found, please use the `", arg_name, "` argument to ",
+      " specify which you want to load.\n", paste(file_names, collapse = ", ")
+    ), call. = FALSE)
+  }
+
+  file_names
+}
