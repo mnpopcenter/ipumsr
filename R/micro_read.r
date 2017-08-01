ff54a07dc984eefe8dfc9bbc99ca377c10871805
gfellis <gfellis@umn.edu>
2017-08-01 13:31:12 -0500

    Add ability to load hierarchical data into lists and nested data.frames

--- R/micro_read.r
+++ R/micro_read.r
@@ -19,6 +19,13 @@
 #'   convetions. For hierarchical data, the rectype id variable will be added even
 #'   if it is not specified.
 #' @param n_max The maximum number of records to load.
+#' @param data_structure For hierarchical data extract, one of "long", "list",
+#'   or "nested" to indicate how to structure the data once loaded. "long" data
+#'   puts all rectypes in the same data.frame, with \code{NA} values for
+#'   variables that do not apply to the rectype. "list" data puts a data.frame
+#'   for each rectype into a list. "nested" creates a single data.frame with one
+#'   row per most-general rectype, with nested data.frames
+#'   that have information for each rectype.
 #' @param data_file Specify a directory to look for the data file.
 #'   If left empty, it will look in the same directory as the DDI file.
 #' @param verbose Logical, indicating whether to print progress information
@@ -29,7 +36,14 @@
 #' }
 #' @family ipums_read
 #' @export
-read_ipums_micro <- function(ddi, vars = NULL, n_max = -1, data_file = NULL, verbose = TRUE) {
+read_ipums_micro <- function(
+  ddi,
+  vars = NULL,
+  n_max = -1,
+  data_structure = c("long", "list", "nested"),
+  data_file = NULL,
+  verbose = TRUE
+) {
   if (is.character(ddi)) ddi <- read_ddi(ddi)
   if (is.null(data_file)) data_file <- file.path(ddi$file_path, ddi$file_name)
   if (!file.exists(data_file) & file.exists(paste0(data_file, ".gz"))) {
@@ -43,9 +57,10 @@ read_ipums_micro <- function(ddi, vars = NULL, n_max = -1, data_file = NULL, ver
   }
 
   vars <- enquo(vars)
+  data_structure <- match.arg(data_structure)
 
   if (ddi$file_type == "hierarchical") {
-    read_ipums_hier(ddi, vars, n_max, data_file, verbose)
+    read_ipums_hier(ddi, vars, n_max, data_structure, data_file, verbose)
   } else if (ddi$file_type == "rectangular") {
     read_ipums_rect(ddi, vars, n_max, data_file, verbose)
   } else {
@@ -54,7 +69,7 @@ read_ipums_micro <- function(ddi, vars = NULL, n_max = -1, data_file = NULL, ver
 
 }
 
-read_ipums_hier <- function(ddi, vars, n_max, data_file, verbose) {
+read_ipums_hier <- function(ddi, vars, n_max, data_structure, data_file, verbose) {
   all_vars <- ddi$var_info
 
   rec_vinfo <- dplyr::filter(all_vars, .data$var_name == ddi$rectype_idvar)
@@ -73,37 +88,91 @@ read_ipums_hier <- function(ddi, vars, n_max, data_file, verbose) {
   lines <- readr::read_lines(data_file, progress = FALSE, n_max = n_max)
 
   if (verbose) cat("Parsing data...\n")
-  nlines <- length(lines)
+  if (data_structure == "long") {
+    nlines <- length(lines)
 
-  # Make a data.frame with all of the variables we will need
-  out <- purrr::map(all_vars$var_type, nlines = nlines, function(.x, nlines) {
-    switch(
-      .x,
-      "numeric" = rep(NA_real_, nlines),
-      "character" = rep(NA_character_, nlines)
+    # Make a data.frame with all of the variables we will need
+    out <- purrr::map(all_vars$var_type, nlines = nlines, function(.x, nlines) {
+      switch(
+        .x,
+        "numeric" = rep(NA_real_, nlines),
+        "character" = rep(NA_character_, nlines)
       )
-  })
-  out <- purrr::set_names(out, all_vars$var_name)
-  out <- tibble::as.tibble(out)
+    })
+    out <- purrr::set_names(out, all_vars$var_name)
+    out <- tibble::as.tibble(out)
 
-  # Add rectype var into our empty data frame
-  out[seq_len(nlines), rec_vinfo$var_name] <-
-    stringr::str_sub(lines, rec_vinfo$start, rec_vinfo$end)
+    # Add rectype var into our empty data frame
+    out[seq_len(nlines), rec_vinfo$var_name] <-
+      stringr::str_sub(lines, rec_vinfo$start, rec_vinfo$end)
 
-  # Add the rest of the variables
-  all_rec_types <- unique(out[[rec_vinfo$var_name]])
-  rec_index <- purrr::map(all_rec_types, ~out[[rec_vinfo$var_name]] == .)
-  rec_index <- purrr::set_names(rec_index, all_rec_types)
+    # Add the rest of the variables
+    all_rec_types <- unique(out[[rec_vinfo$var_name]])
+    rec_index <- purrr::map(all_rec_types, ~out[[rec_vinfo$var_name]] == .)
+    rec_index <- purrr::set_names(rec_index, all_rec_types)
 
-  purrr::pwalk(nonrec_vinfo, function(var_name, start, end, imp_decim, var_type, rectypes, ...) {
-    var_data <- stringr::str_sub(lines[rec_index[[rectypes]]], start, end)
-    if (var_type == "numeric") {
-      var_data <- as.numeric(var_data)
+    purrr::pwalk(nonrec_vinfo, function(var_name, start, end, imp_decim, var_type, rectypes, ...) {
+      var_data <- stringr::str_sub(lines[rec_index[[rectypes]]], start, end)
+      if (var_type == "numeric") {
+        var_data <- as.numeric(var_data)
+      }
+      out[rec_index[[rectypes]], var_name] <<- var_data
+    })
+
+    out <- set_ipums_var_attributes(out, all_vars)
+  } else if (data_structure == "list" | data_structure == "nested") {
+    # Determine rectypes
+    rec_type <- stringr::str_sub(lines, rec_vinfo$start, rec_vinfo$end)
+    rec_types_in_extract <- dplyr::intersect(rec_vinfo$rectypes[[1]], unique(rec_type))
+
+    # Make a data.frame for each rectype
+    out <- purrr::map(rec_types_in_extract, function(rt) {
+      vars_in_rec <- nonrec_vinfo[purrr::map_lgl(nonrec_vinfo$rectypes, ~rt %in% .), ]
+      lines_in_rec <- lines[rec_type == rt]
+
+      nlines_rt <- length(lines_in_rec)
+
+      out_rt <- purrr::map(vars_in_rec$var_type, nlines = nlines_rt, function(.x, nlines) {
+        switch(
+          .x,
+          "numeric" = rep(NA_real_, nlines),
+          "character" = rep(NA_character_, nlines)
+        )
+      })
+      out_rt <- purrr::set_names(out_rt, vars_in_rec$var_name)
+      out_rt <- tibble::as.tibble(out_rt)
+
+
+      # Add in the variables
+      purrr::pwalk(vars_in_rec, function(var_name, start, end, imp_decim, var_type, rectypes, ...) {
+        var_data <- stringr::str_sub(lines_in_rec, start, end)
+        if (var_type == "numeric") {
+          var_data <- as.numeric(var_data)
+        }
+        out_rt[[var_name]] <<- var_data
+      })
+
+      out_rt <- set_ipums_var_attributes(out_rt, vars_in_rec)
+    })
+    names(out) <- rec_types_in_extract
+
+    if (data_structure == "nested") {
+      # Work backwards in rectypes to start with the most nested
+      rectypes_to_nest <- rev(rec_types_in_extract[-1])
+
+      for (rt in rectypes_to_nest) {
+        next_rt <- rec_types_in_extract[which(rec_types_in_extract == rt) - 1]
+
+        merge_vars <- dplyr::intersect(names(out[[rt]]), names(out[[next_rt]]))
+        nest_vars <- dplyr::setdiff(names(out[[rt]]), merge_vars)
+
+        out[[rt]] <- tidyr::nest_(out[[rt]], rt, nest_vars)
+        out[[next_rt]] <- dplyr::full_join(out[[next_rt]], out[[rt]], by = merge_vars)
+        out[[rt]] <- NULL
+      }
+      out <- out[[rec_types_in_extract[1]]]
     }
-    out[rec_index[[rectypes]], var_name] <<- var_data
-  })
-
-  out <- set_ipums_var_attributes(out, all_vars)
+  }
   out
 }
 
