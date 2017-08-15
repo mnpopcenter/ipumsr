1c74adfc33e8859719204af37eb8ec2077ea4676
gfellis <gfellis@umn.edu>
2017-08-15 09:53:11 -0500

    More NHGIS tests

--- tests/testthat/test_nhgis.r
+++ tests/testthat/test_nhgis.r
@@ -61,3 +61,29 @@ test_that(
     expect_equal(sort(nhgis$PMSA)[1:2], pmsa_first2_sort)
     expect_equal(class(nhgis), c("sf", "tbl_df", "tbl", "data.frame"))
   })
+
+
+test_that(
+  "Can read NHGIS extract (with shape as sf - 2 layers unzipped)", {
+    temp_dir <- tempfile()
+    dir.create(temp_dir)
+    utils::unzip(ripums_example("nhgis0008_csv.zip"), exdir = temp_dir)
+    utils::unzip(ripums_example("nhgis0008_shape.zip"), exdir = temp_dir)
+    utils::unzip(
+      file.path(temp_dir, "nhgis0008_shape/nhgis0008_shapefile_tl2000_us_pmsa_1990.zip"),
+      exdir = temp_dir
+    )
+
+    nhgis <- read_nhgis(
+      file.path(temp_dir, "nhgis0008_csv/nhgis0008_ds135_1990_pmsa.csv"),
+      file.path(temp_dir, "US_pmsa_1990.shp"),
+      verbose = FALSE
+    )
+
+    expect_equal(nrow(nhgis), rows)
+    expect_equal(ncol(nhgis), vars_data_shape)
+    expect_equal(attr(nhgis[["D6Z001"]], "label"), d6z001_label)
+    expect_equal(attr(nhgis[["D6Z001"]], "var_desc"), d6z001_var_desc)
+    expect_equal(sort(nhgis$PMSA)[1:2], pmsa_first2_sort)
+    expect_equal(class(nhgis), c("sf", "tbl_df", "tbl", "data.frame"))
+  })
