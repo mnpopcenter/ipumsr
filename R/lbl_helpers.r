23dff80dc0a3ae21603cac5964cf2f04bbf660b7
gfellis <gfellis@umn.edu>
2017-09-13 10:53:06 -0500

    More work for #7 - lbl_collapse

--- R/lbl_helpers.r
+++ R/lbl_helpers.r
@@ -54,6 +54,69 @@ lbl_na_if <- function(x, .predicate) {
   out
 }
 
+#' Collapse labelled values to labels that already exist
+#'
+#' Converts values to a new value based on their label and value in a
+#' \code{\link[haven]{labelled}} vector. If the newly assigned value does
+#' not match an already existing labelled value, the smallest value's label
+#' is used. Ignores any value that does not have a label.
+#'
+#' @param x A \code{\link[haven]{labelled}} vector
+#' @param .fun A function that takes .val and .lbl (the values and
+#'    labels) and returns the values of the label you want to change it to.
+#'    It is passed to a function similar to \code{\link[rlang]{as_function}}, so
+#'    also accepts quosure-style lambda functions (that use values .val and .lbl).
+#' @return A haven::labeled vector
+#' @examples
+#' x <- haven::labelled(
+#'   c(10, 10, 11, 20, 30, 99, 30, 10),
+#'   c(Yes = 10, `Yes - Logically Assigned` = 11, No = 20, Maybe = 30, NIU = 99)
+#' )
+#'
+#' lbl_collapse(x, ~(.val %/% 10) * 10)
+#' # Notice that 90 get's NIU from 99 even though 90 didn't have a label in original
+#'
+#' lbl_collapse(x, ~ifelse(.lbl == 10, 11, .lbl))
+#' # But here 10 is assigned 11's label
+#'
+#' # You can also use the more explicit function notation
+#' lbl_collapse(x, function(.val, .lbl) (.val %/% 10) * 10)
+#'
+#' # Or even the name of a function
+#' collapse_function <- function(.val, .lbl) (.val %/% 10) * 10
+#' lbl_na_if(x, "collapse_function")
+#'
+#' @family lbl_helpers
+#' @export
+lbl_collapse <- function(x, .fun) {
+  pred_f <- as_lbl_function(.fun, caller_env())
+
+  old_attributes <- attributes(x)
+
+  label_info <- dplyr::data_frame(
+    old_val = unname(old_attributes$labels),
+    old_label = names(old_attributes$labels),
+    new_val = pred_f(.val = old_val, .lbl = old_label),
+    vals_equal = old_val == new_val
+  )
+  # Arrange so that if value existed in old values it is first, otherwise first old value
+  label_info <- dplyr::group_by(label_info, .data$new_val)
+  label_info <- dplyr::arrange(label_info, .data$new_val, dplyr::desc(.data$vals_equal), .data$old_val)
+  label_info <- dplyr::mutate(label_info, new_label = .data$old_label[1])
+  label_info <- dplyr::ungroup(label_info)
+
+  new_labels <- dplyr::select(label_info, dplyr::one_of(c("new_label", "new_val")))
+  new_labels <- dplyr::distinct(new_labels)
+  new_labels <- tibble::deframe(new_labels)
+  new_attributes <- old_attributes
+  new_attributes$labels <- new_labels
+
+  out <- label_info$new_val[match(x, label_info$old_val)]
+
+  attributes(out) <- new_attributes
+  out
+}
+
 
 # Based on rlang::as_function
 # Changed so that instead of function having args .x & .y, it has
--- tests/testthat/test_lbls.r
+++ tests/testthat/test_lbls.r
@@ -1,4 +1,4 @@
-context("label helpers: lbl_na_if")
+context("label helpers")
 
 x <- haven::labelled(
   c(10, 10, 11, 20, 30, 99, 30, 10),
@@ -12,14 +12,14 @@ x_na <- haven::labelled(
 )
 attr(x_na, "label") <- "Test label"
 
-test_that("Simple example with both .lbl and .val keywords", {
+test_that("lbl_na_if: Simple example with both .lbl and .val keywords", {
   expect_equal(
     lbl_na_if(x, ~.val >= 90 | .lbl %in% c("Maybe")),
     x_na
   )
 })
 
-test_that("Respects environment", {
+test_that("lbl_na_if: Respects environment", {
   upper_val <- 90
   expect_equal(
     lbl_na_if(x, ~.val >= upper_val | .lbl %in% c("Maybe")),
@@ -27,17 +27,59 @@ test_that("Respects environment", {
   )
 })
 
-test_that("Explicit function", {
+test_that("lbl_na_if: Explicit function", {
   expect_equal(
     lbl_na_if(x, function(.val, .lbl) .val >= 90 | .lbl %in% c("Maybe")),
     x_na
   )
 })
 
-test_that("Function name", {
+test_that("lbl_na_if: Function name", {
   test_f <- function(.val, .lbl) .val >= 90 | .lbl %in% c("Maybe")
   expect_equal(
     lbl_na_if(x, "test_f"),
     x_na
   )
 })
+
+test_that("lbl_collapse: basic", {
+  x_basic_collapse <- haven::labelled(
+    c(10, 10, 10, 20, 30, 90, 30, 10),
+    c(Yes = 10, No = 20, Maybe = 30, NIU = 90)
+  )
+  attr(x_basic_collapse, "label") <- "Test label"
+
+  expect_equal(
+    lbl_collapse(x, ~(.val %/% 10) * 10),
+    x_basic_collapse
+  )
+})
+
+test_that("lbl_collapse: if recoding to old value it is maintained (even if it's not first)", {
+  x_basic_collapse <- haven::labelled(
+    c(11, 11, 11, 20, 30, 99, 30, 11),
+    c(`Yes - Logically Assigned` = 11, No = 20, Maybe = 30, NIU = 99)
+  )
+  attr(x_basic_collapse, "label") <- "Test label"
+
+  expect_equal(
+    lbl_collapse(x, ~ifelse(.val == 10, 11, .val)),
+    x_basic_collapse
+  )
+})
+
+# test_that("lbl_relabel: basic", {
+#   x_basic_relabel <- haven::labelled(
+#     c(10, 10, 10, 20, 30, 90, 30, 10),
+#     c(Yes = 10, No = 20, `Garbage codes` = 90)
+#   )
+#   attr(x_basic_relabel, "label") <- "Test label"
+#
+#   expect_equal(
+#     lbl_relabel(x, lbl(90, "Garbage codes") ~ .val == 99 | .lbl == "Maybe"),
+#     x_basic_relabel
+#   )
+# })
+
+
+#test_that("lbl_add: ")
