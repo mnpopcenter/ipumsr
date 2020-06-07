6ce7d18720a3a8e08028cdf687bd9cdf877d4f1d
Greg Freedman Ellis <greg@crunch.io>
2020-06-06 20:29:50 -0500

    use haven::labelled from Rcpp to create labelled vectors

--- src/set_attributes.cpp
+++ src/set_attributes.cpp
@@ -9,9 +9,14 @@ DataFrame set_ipums_var_attributes_(DataFrame x, DataFrame var_info) {
   CharacterVector var_labels = var_info["var_label"];
   CharacterVector var_descs = var_info["var_desc"];
   List val_labels = var_info["val_labels"];
+  Function labelled_f = Environment::namespace_env("haven")["labelled"];
 
   for (int i = 0; i < num_vars; i++) {
     const char* vn = var_names[i];
+    if (!Rf_isNull(val_labels[i])) {
+      x[vn] = labelled_f(x[vn], val_labels[i]);
+    }
+
     SEXP col = x[vn];
 
     SEXP vl = PROTECT(Rf_allocVector(STRSXP, 1));
@@ -23,15 +28,6 @@ DataFrame set_ipums_var_attributes_(DataFrame x, DataFrame var_info) {
     SET_STRING_ELT(vd, 0, var_descs[i]);
     Rf_setAttrib(col, Rf_install("var_desc"), vd);
     UNPROTECT(1);
-
-    if (!Rf_isNull(val_labels[i])) {
-      SEXP new_class = PROTECT(Rf_allocVector(STRSXP, 1));
-      SET_STRING_ELT(new_class, 0, Rf_mkChar("haven_labelled"));
-      Rf_setAttrib(col, Rf_install("class"), new_class);
-      UNPROTECT(1);
-
-      Rf_setAttrib(col, Rf_install("labels"), val_labels[i]);
-    }
   }
   return x;
 }
